3d303af4776c23f99812853e01bfa6125c791906
“Gabe <gabeharris@gmail.com>
2014-07-16 21:08:29 -0400

    styles

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-	stylus = require('stylus');
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
@@ -11,6 +13,8 @@ function compile(str, path){
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
 app.use(stylus.middleware(
 	{
 		src: __dirname + '/public',
